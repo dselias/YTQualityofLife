034ed5d2f839fa5cbe228971890ae8522bfed125
dselias <29785405+dselias@users.noreply.github.com>
2022-06-10 18:06:53 +0200

    PlaylistAutoplayDisabled: simplified backend and fixed all bugs. Now works on all types of videos, playlists and queues.

--- scripts/contentScripts/features/PlaylistAutoplayDisabled.js
+++ scripts/contentScripts/features/PlaylistAutoplayDisabled.js
@@ -1,74 +1,23 @@
-let currentTimeElement;
 let videoElement;
-let currentTime = 5;
-let totalTime = 0;
-let timerActive = false;
+let alreadyInitialized = false;
 
 const PlaylistAutoplayDisabledSetup = () => {
+    if (alreadyInitialized) return;
+    alreadyInitialized = true;
+
     console.log("PlaylistAutoplayDisabled Enabled");
-    currentTimeElement = document.getElementsByClassName("ytp-time-current")[0];
     videoElement = document.getElementsByTagName("video")[0];
-    totalTime = convertToSeconds(document.getElementsByClassName("ytp-time-duration")[0].innerHTML);
-    setPlaylistAutoplayDisabledObservers();
-    checkVideoStatus();
-}
 
-const setPlaylistAutoplayDisabledObservers = () => {
-    //observer to detect playtime change
-    new MutationObserver((mutationsList) => {
-        currentTime = convertToSeconds(mutationsList[0].target.innerText);
-    }).observe(currentTimeElement, { characterData: true, childList: true });
-}
+    setInterval(() => {
+        if (videoElement == null) videoElement = document.getElementsByTagName("video")[0];
+        let totalTime = videoElement.duration;
+        let currentTime = videoElement.currentTime;
+        let timeLeft = totalTime - currentTime;
 
-const checkVideoStatus = () => {
-    checkTimeLeft();
-    timerActive = true;
+        // console.log(timeLeft);
 
-    videoElement.addEventListener("play", (event) => {
-        console.log(event)
-        if (timerActive == false) {
-            timerActive = true;
-            checkTimeLeft();
+        if (timeLeft <= 2) {
+            videoElement.pause();
         }
-    });
-
-    videoElement.addEventListener("pause", () => {
-        timerActive = false;
-    });
-}
-
-const checkTimeLeft = () => {
-    setTimeout(() => {
-        let timeLeftInSeconds = totalTime - currentTime;
-
-        // console.log("totalTime: " + totalTime);
-        // console.log("currentTime: " + currentTime);
-        console.log("timeLeft: " + timeLeftInSeconds);
-        currentTime++;
-
-        // if (timeLeftInSeconds <= 2) {
-        //     videoElement.pause();
-        //     timerActive = false;
-        // }
-
-        if (timerActive) checkTimeLeft();
     }, 1000);
-}
-
-const convertToSeconds = (time) => {
-    let totalSeconds = 0;
-
-    let splitTimeStatus = time.split(":").reverse();
-
-    for (let i = 0; i < splitTimeStatus.length; i++) {
-        if (i == 0) {
-            totalSeconds += parseInt(splitTimeStatus[i].trim());
-        } else if (i == 1) {
-            totalSeconds += parseInt(splitTimeStatus[i].trim()) * (60 * i);
-        } else {
-            totalSeconds += parseInt(splitTimeStatus[i].trim()) * (3600 * i);
-        }
-    }
-
-    return totalSeconds;
 }
