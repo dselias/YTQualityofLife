fa1eda5791e097651b7f008048cc494ed0c425be
dselias <29785405+dselias@users.noreply.github.com>
2022-06-11 02:10:12 +0200

    PlaylistRemovePopup: Queue bugfix

--- scripts/contentScripts/features/PlaylistRemovePopup.js
+++ scripts/contentScripts/features/PlaylistRemovePopup.js
@@ -15,7 +15,10 @@ const changeDotsToTrashcan = (videos) => {
     for (let i = 0; i < videos.length; i++) {
         //location of three-dots-vector icon
         let vector = videos[i].getElementsByTagName("path")[1];
-        vector.setAttribute("d", "M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z");
+        
+        if(vector != null) {
+            vector.setAttribute("d", "M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z");
+        } 
     }
 }
 
